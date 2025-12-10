618b2af47bd2bf93b2e99e8c6e5d7338c511a77b
nowaljin <nowaljin13@gmail.com>
2025-12-10 12:35:30 +0900

    update

--- Assets/NewMonoBehaviourScript.cs
+++ Assets/NewMonoBehaviourScript.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
+using System.Linq;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        float[] array = new float[10];
+        var list0 = array.ToList();
     }
 
     // Update is called once per frame
